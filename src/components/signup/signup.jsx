0dc0f7b56ea5276583b73559f91b2706d02ff74f
Abhishek Prasad <34980034+abhpd@users.noreply.github.com>
2021-03-25 00:05:13 +0530

    Revert "login, signup, landing"

--- src/components/signup/signup.jsx
+++ src/components/signup/signup.jsx
@@ -1,112 +1,61 @@
 import React from "react";
-import stylesCSS from "./styles.module.css";
-import logo from "../resources/BiggerLogo2.png";
 
-class Signup extends React.Component {
-  constructor(props) {
-    super(props);
+class Signup extends React.Component{
+    constructor(props){
+        super(props);
 
-    this.state = {
-      username: "",
-      email: "",
-      password: "",
-      repeatPassword: "",
-    };
-    this.formChange = this.formChange.bind(this);
-    this.signupButtonHandler = this.signupButtonHandler.bind(this);
-  }
-
-  formChange() {
-    this.setState({
-      username: document.getElementById("username").value,
-      email: document.getElementById("email").value,
-      password: document.getElementById("password").value,
-      repeatPassword: document.getElementById("repeatpassword").value,
-    });
-  }
-
-  async signupButtonHandler() {
-    if (this.state.password === this.state.repeatPassword) {
-      console.log(this.state.username);
-      console.log(this.state.email);
-      console.log(this.state.password);
-      console.log(this.state.repeatPassword);
-
-      const response = await fetch(
-        process.env.REACT_APP_API_URI + "/login/signup",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(this.state),
+        this.state = {
+            username:"",
+            email:"",
+            password:"",
+            repeatPassword:""
         }
-      );
-
-      const data = await response.json();
-
-      console.log(data.message);
+        this.formChange = this.formChange.bind(this);
+        this.signupButtonHandler = this.signupButtonHandler.bind(this);
     }
-  }
 
-  render() {
-    return (
-      <div className={stylesCSS.signup}>
-        <div className={stylesCSS.signupcontainer}>
-          <form action="">
-            <img className={stylesCSS.logo} src={logo} alt="logo" />
-            <h1>Examiner Signup</h1>
-            <div className={stylesCSS.txtfield}>
-              <input
-                type="text"
-                name=""
-                placeholder="Username"
-                id="username"
-                required
-                onChange={this.formChange}
-              />
+    formChange(){
+        this.setState({
+            username: document.getElementById("username").value,
+            email: document.getElementById("email").value,
+            password: document.getElementById("password").value,
+            repeatPassword: document.getElementById("repeatpassword").value,
+        });
+    }
+
+    async signupButtonHandler(){
+        if(this.state.password===this.state.repeatPassword){
+            console.log(this.state.username);
+            console.log(this.state.email);
+            console.log(this.state.password);
+            console.log(this.state.repeatPassword);
+
+            const response = await fetch(process.env.REACT_APP_API_URI + "/login/signup",{
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(this.state)
+            });
+
+            const data = await response.json();
+
+            console.log(data.message);
+        }
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>Signup</h1>
+                <p>Username: </p><input type="text" name="" id="username" required onChange={this.formChange}/>
+                <p>Email: </p><input type="text" name="" id="email" required onChange={this.formChange}/>
+                <p>Password: </p><input type="text" name="" id="password" required onChange={this.formChange}/>
+                <p>Repeat Password: </p><input type="text" name="" id="repeatpassword" required onChange={this.formChange}/>
+                <button onClick={this.signupButtonHandler}>Signup</button>
             </div>
-            <div className={stylesCSS.txtfield}>
-              <input
-                type="text"
-                name=""
-                placeholder="Email"
-                id="email"
-                required
-                onChange={this.formChange}
-              />
-            </div>
-            <div className={stylesCSS.txtfield}>
-              <input
-                type="text"
-                name=""
-                placeholder="Password"
-                id="password"
-                required
-                onChange={this.formChange}
-              />
-            </div>
-            <div className={stylesCSS.txtfield}>
-              <input
-                type="text"
-                name=""
-                placeholder="Repeat Password"
-                id="repeatpassword"
-                required
-                onChange={this.formChange}
-              />
-            </div>
-            <button
-              className={stylesCSS.signupbutton}
-              onClick={this.signupButtonHandler}
-            >
-              Signup
-            </button>
-          </form>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default Signup;

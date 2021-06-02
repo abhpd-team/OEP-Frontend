1cfc182e27ca5eceda5bfcdcfbb581f0ee336632
19BCS1114 <19bcs1114@gmail.com>
2021-06-02 13:32:18 +0530

    Responsive classes

--- src/components/modules/searchBar/searchBar.jsx
+++ src/components/modules/searchBar/searchBar.jsx
@@ -1,9 +1,40 @@
-import stylesCSS from "./styles.module.css"
+import stylesCSS from "./styles.module.css";
+import { useState } from "react";
 
-export default function SearchBar (props){
-    return (
-        <div className={stylesCSS.searchBarContainer}>
-            <input className={stylesCSS.searchBar} type="text" placeholder={props.placeholder}/>
-        </div>
-    )
+export default function SearchBar(props) {
+  const [state, setState] = useState({ cardName: "" });
+  const searchHandler = (event) => {
+    setState({ cardName: event.target.value });
+    let cards = document.getElementsByClassName("cardS");
+    if (event.target.value !== "") {
+      for (let i = 0; i < cards.length; i++) {
+        if (
+          cards[
+            i
+          ].firstElementChild.lastElementChild.firstElementChild.innerHTML
+            .toLowerCase()
+            .search(event.target.value.toLowerCase()) === -1
+        ) {
+          cards[i].classList.add("d-none");
+        } else {
+          cards[i].classList.remove("d-none");
+        }
+      }
+    } else {
+      for (let i = 0; i < cards.length; i++) {
+        cards[i].classList.remove("d-none");
+      }
+    }
+  };
+  return (
+    <div className={stylesCSS.searchBarContainer}>
+      <input
+        className={stylesCSS.searchBar}
+        type="text"
+        placeholder={props.placeholder}
+        value={state.cardName}
+        onChange={searchHandler}
+      />
+    </div>
+  );
 }
--- src/components/modules/smallCard/smallCard.jsx
+++ src/components/modules/smallCard/smallCard.jsx
@@ -1,4 +1,4 @@
-import stylesCSS from "./styles.module.css"
+import stylesCSS from "./styles.module.css";
 
 // props{
 //     header
@@ -6,18 +6,20 @@ import stylesCSS from "./styles.module.css"
 //     deleteHandler - function to handle delete operation
 // }
 
-export default function SmallCard (props){
-    return (
-        <div className={stylesCSS.card}>
-            <div className={stylesCSS.cardHeader}>
-                <div className={stylesCSS.deleteCard} onClick={props.deleteHandler}>
-                    <p className={stylesCSS.deleteCross}><i className="fas fa-times"></i></p>
-                </div>
-                <a href={props.href}><h1>{props.header}</h1></a>
-            </div>
-            <div className={stylesCSS.cardFooter}>
-                <p>{props.footer}</p>
-            </div>
+export default function SmallCard(props) {
+  return (
+    <div className={stylesCSS.card + " cardS"}>
+      <div className={stylesCSS.cardHeader}>
+        <div className={stylesCSS.deleteCard} onClick={props.deleteHandler}>
+          <p className={stylesCSS.deleteCross}>
+            <i className="fas fa-2x fa-times"></i>
+          </p>
         </div>
-    )
+        <a href={props.href}>
+          <h1>{props.header}</h1>
+        </a>
+      </div>
+      <div className={stylesCSS.cardFooter}>{props.footer}</div>
+    </div>
+  );
 }
